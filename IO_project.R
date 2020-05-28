d827265902679810c1fcfc26756ec8e009a2132a
Nikita <52459271+NBar05@users.noreply.github.com>
2020-05-28 19:34:40 +0300

    Update IO_project.R

--- IO_project.R
+++ IO_project.R
@@ -30,10 +30,11 @@ naming <- "media_news/news-%s.csv" %--% list(c(1:21))
 media_news <- data.frame()
 for (i in 1:length(naming)) {
   data <- read.csv(naming[i], header = TRUE, na.strings = "", stringsAsFactors = FALSE)
-  data$Период <- as.character(dates[i])
+  data$Период <- dates[i]
   media_news <- rbind(media_news, data)
 }
 
+media_news$Период <- as.yearmon(media_news$Период)
 # не будем искать врагов там, где их нет
 media_news_c <- media_news %>%
   filter(Тематики.ресурса !=
@@ -44,21 +45,32 @@ media_news_c <- media_news %>%
       "Техника и технологии, Новости и СМИ->Деловые новости, Техника и технологии->Новости Hi-Tech, Бизнес, Новости и СМИ, Бизнес->Деловые новости")
 
 # оставим только газеты и журналы
-media_only_news <- media_news %>% filter(Тематики.ресурса == "Новости и СМИ, Новости и СМИ->Газеты, журналы")
+# media_only_news <- media_news %>% filter(Тематики.ресурса == "Новости и СМИ, Новости и СМИ->Газеты, журналы")
 
 
 # 
 media_holdings <- media_holdings %>% mutate(year = as.integer(format(as.Date(Период), "%Y")), 
                                             month = as.integer(format(as.Date(Период), "%m")))
 
-names(media_holdings)[1:6] <- c("name", "cross_device_visitors", "users", "daily_audience", "average_time", "date")
+names(media_holdings)[1:6] <- c("name", "cross_device_visitors", "users", "daily_audience", "avg_time", "date")
 
 summary_holdings <- media_holdings %>% group_by(name, year) %>% summarise(yearly_audience = sum(cross_device_visitors),
                                                                           counts = n()) %>% filter(year == 2019)
 
 summary_holdings$gov <- c(1, 0, 1, 1, 0, 0, 1, NA, 1, NA, 0, 0, 1, 1, 0, 0, 0, 0, 0, NA, 1)
 
-summary_holdings <- summary_holdings %>% arrange(gov, desc(yearly_audience))
+summary_holdings_fin <- summary_holdings %>% arrange(gov, desc(yearly_audience)) %>% 
+  filter(!(name %in% c()))
 
-stargazer(summary_holdings, type = "latex", summary = FALSE)
+# stargazer(summary_holdings, type = "latex", summary = FALSE)
+
+names(media_news_c) <- c("name", "url", "theme", "type", "holding", "metrics", "cross_device_visitors",
+                         "users", "avg_time", "mobile_audience", "daily_audience", "date")
+
+summary_media_news <- media_news_c %>% mutate(year = as.integer(format(as.Date(date), "%Y")), 
+                                              month = as.integer(format(as.Date(date), "%m"))) %>%
+                      group_by(year, name) %>% summarise(yearly_audience = sum(cross_device_visitors),
+                                                            counts = n()) %>% filter(year == 2019) %>%
+                      filter(counts == 12) %>% mutate(share = yearly_audience / sum(yearly_audience),
+                                                      share_2 = share^2) %>% summarise(H = sum(share_2)*10000)
 
